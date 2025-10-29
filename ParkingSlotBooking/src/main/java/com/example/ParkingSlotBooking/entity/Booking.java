915e8f483309e950a3179de28259f77879d4c9aa
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-10-29 13:01:13 +0530

    feat(Bookings) : Add booking entity model with attributes userid,slotid,starttime,endtime,status,amount,createdAt

--- ParkingSlotBooking/src/main/java/com/example/ParkingSlotBooking/entity/Booking.java
+++ ParkingSlotBooking/src/main/java/com/example/ParkingSlotBooking/entity/Booking.java
@@ -1,5 +1,35 @@
 package com.example.ParkingSlotBooking.entity;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Booking {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "slot_id", nullable = false)
+    private Slot slot;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String status;
+    private double amount;
+    private LocalDateTime createdAt;
 }
+
