cdc60bc6b12f41619f4ed9e3696e5fbd51b3d794
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-10-29 10:37:46 +0530

    fix : comment foreign key mapping

--- ParkingSlotBooking/src/main/java/com/example/ParkingSlotBooking/entity/Slot.java
+++ ParkingSlotBooking/src/main/java/com/example/ParkingSlotBooking/entity/Slot.java
@@ -27,39 +27,3 @@ public class Slot {
 
 }
 
-/*package com.ecommerce.wishlist.entity;
-
-import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.Data;
-import java.util.List;
-import jakarta.persistence.CascadeType;
-
-@Entity
-@Data
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "userId")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
-
-    private String name;
-    private String email;
-    private String notificationPreference;
-    private LocalDateTime createdAt = LocalDateTime.now();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    @JsonManagedReference(value = "user-wishlist")
-    private List<Wishlist> wishlistItems;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Notification> notifications;
-}
-*/
