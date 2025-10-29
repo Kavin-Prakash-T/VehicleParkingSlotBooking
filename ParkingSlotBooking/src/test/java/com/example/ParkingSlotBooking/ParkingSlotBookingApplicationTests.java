ef7ea6f63d7ea6ea6e74881772f4d03200ded0a6
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-10-29 09:38:11 +0530

    chore : Add Dependencies

---
+++ ParkingSlotBooking/src/test/java/com/example/ParkingSlotBooking/ParkingSlotBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParkingSlotBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSlotBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
