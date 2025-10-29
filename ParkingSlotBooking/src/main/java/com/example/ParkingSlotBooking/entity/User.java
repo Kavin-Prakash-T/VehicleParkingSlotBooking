b3f81ceca9e2be08f2559f8f3abc5834b789aff9
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-10-29 10:22:49 +0530

    feat(User) : Add User Enitity - User Entity model created using id ,name,emai, and created time

--- ParkingSlotBooking/src/main/java/com/example/ParkingSlotBooking/entity/User.java
+++ ParkingSlotBooking/src/main/java/com/example/ParkingSlotBooking/entity/User.java
@@ -1,5 +1,28 @@
 package com.example.ParkingSlotBooking.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+    private String email;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    /*@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Booking> bookings;*/
 
 }
